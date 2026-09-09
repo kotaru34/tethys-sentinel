b1c61735f9167fad9dcccb7f2aa614ba73f681c1
Kotaru <120008285+kotaru34@users.noreply.github.com>
2026-09-10 01:11:05 +0200

    test: assert PostgreSQL port conformance

---
+++ internal/postgresrepo/ports_test.go
@@ -0,0 +1,21 @@
+package postgresrepo
+
+import (
+	"github.com/kotaru34/tethys-sentinel/internal/capability"
+	"github.com/kotaru34/tethys-sentinel/internal/controlapi"
+	"github.com/kotaru34/tethys-sentinel/internal/credentialapi"
+	"github.com/kotaru34/tethys-sentinel/internal/emergencyapi"
+	"github.com/kotaru34/tethys-sentinel/internal/resourceapi"
+)
+
+var (
+	_ capability.Backend        = (*CapabilityBackend)(nil)
+	_ controlapi.ApprovalStore  = (*ApprovalStore)(nil)
+	_ controlapi.AuditStore     = (*AuditStore)(nil)
+	_ controlapi.JobStore       = (*JobStore)(nil)
+	_ credentialapi.JobStore    = (*JobStore)(nil)
+	_ credentialapi.AuditStore  = (*AuditStore)(nil)
+	_ emergencyapi.Controller   = (*EmergencyController)(nil)
+	_ resourceapi.AuditStore    = (*AuditStore)(nil)
+	_ resourceapi.NoteStore     = (*NoteStore)(nil)
+)
